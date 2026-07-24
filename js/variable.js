55628abf7630df41e914267007df38f3a29643e5
rufaida akhter akhi <rufaida.formal@gmail.com>
2026-07-24 11:30:32 +0600

    git js push

---
+++ js/variable.js
@@ -0,0 +1,10 @@
+// variable
+var weigth=38;
+var tempereture=28;
+var people = 37 ;
+console.log(weigth);
+console.log(tempereture);
+let name = "akhi"
+console.log(name);
+name="rufaida akhter akhi";
+console.log(name);
