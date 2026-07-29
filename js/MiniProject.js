c32d057a6d26fe04a199f11c576aa20dd390650c
rufaida akhter akhi <rufaida.formal@gmail.com>
2026-07-29 09:47:39 +0600

    git js push

---
+++ js/MiniProject.js
@@ -0,0 +1,42 @@
+
+let fullName = "John Doe";
+
+let age = 22;
+
+let country = "Bangladesh";
+
+let favoriteLanguage = "JavaScript";
+
+let dreamJob = "Full-Stack Developer";
+
+let yearsToGoal = 1;
+
+let isLearningJS = true;
+
+ 
+
+console.log("========================");
+
+console.log("     MY PROFILE");
+
+console.log("========================");
+
+console.log();
+
+console.log("Name:", fullName);
+
+console.log("Age:", age);
+
+console.log("Country:", country);
+
+console.log("Favorite Language:", favoriteLanguage);
+
+console.log("Dream Job:", dreamJob);
+
+console.log("Years to Reach Goal:", yearsToGoal);
+
+console.log("Learning JavaScript:", isLearningJS);
+
+console.log();
+
+console.log("========================");
