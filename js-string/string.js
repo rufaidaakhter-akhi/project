8333d589b2760dd50c2bc5a3f37d58ae09c11b4e
rufaida akhter akhi <rufaida.formal@gmail.com>
2026-08-01 19:21:40 +0600

    commit

---
+++ js-string/string.js
@@ -0,0 +1,12 @@
+const cup = 'wORLD cUP';
+const tea = "Tea cup";
+const location = `Location: Qatar`;
+console.log(cup); // wORLD cUP
+console.log(tea); // Tea cup
+console.log(location); // Location: Qatar 
+
+const number = [2, 4, 6, 8, 10];
+console.log('cup leangth is :' + cup.length);
+
+
+console.log(number);
