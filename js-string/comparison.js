72c89c0d3ae616dd6bf557bf8797efd2ac22fd0d
rufaida akhter akhi <rufaida.formal@gmail.com>
2026-08-01 19:31:18 +0600

    commit

---
+++ js-string/comparison.js
@@ -0,0 +1,14 @@
+const subject = "chemistry";
+const book = "Chemistry";
+
+
+
+console.log(book.toLowerCase());
+
+if (subject === book) {
+    console.log("poira poira fatai felbo");
+
+}
+else {
+    console.log("ebar ki auto pass ache !!!!!");
+}
