45fd7c5877852f2308edf427e8c1b68abac70e78
rufaida akhter akhi <rufaida.formal@gmail.com>
2026-08-01 18:26:14 +0600

    commit

---
+++ js-string/recap.js
@@ -0,0 +1,9 @@
+const goals = 4;
+const player= "messi";
+const isGolden = true;
+
+if(player ==="mbappe"){
+    console.log("ki khwaise tor bapay");
+}
+
+const matches = ['vs algerisa', 'vs france', 'vs argentina', 'vs brazil'];
