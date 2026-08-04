1b96ba0c7aa5c0aa72a4005449203215d6e178e2
rufaida akhter akhi <rufaida.formal@gmail.com>
2026-08-04 08:50:38 +0600

    function.js

---
+++ js/function/function.js
@@ -0,0 +1,37 @@
+
+/**
+ * function
+ * give a name
+ * give to  bracket
+ * after first bracket give two second bracket
+ * inside the sceond bracket you will do the work
+ * 
+ * 
+ * 
+ * function declaration--------------------------------
+ * 
+ * 
+ * 
+ * 
+
+**/
+
+
+function fanOnKor(){
+    console.log("Fan is on");
+}
+
+
+
+// --------------------------------------------------------
+
+function geet(name){
+    console.log("hi "+name)
+}
+
+// ---------------------------------------------------------
+// call the function -----------------------
+// wright the function name and give the brckets
+
+geet("akhi");
+fanOnKor();
