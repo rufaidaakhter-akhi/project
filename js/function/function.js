3fd932738e01dff9d31a96ca7697a5f4634e3b0a
rufaida akhter akhi <rufaida.formal@gmail.com>
2026-08-04 18:44:41 +0600

    function typeof

--- js/function/function.js
+++ js/function/function.js
@@ -29,9 +29,15 @@ function geet(name){
     console.log("hi "+name)
 }
 
+function sum(a,b){
+    console.log(a+b);
+}
+
 // ---------------------------------------------------------
 // call the function -----------------------
 // wright the function name and give the brckets
-
+sum(100,500);
 geet("akhi");
 fanOnKor();
+console.log("say type of sum " + typeof sum);
+console.log("say type of geet " + typeof geet);
