a6f961b9ba08be7501f818c5d091ca70164e0698
rufaida akhter akhi <rufaida.formal@gmail.com>
2026-08-05 09:50:53 +0600

    return

---
+++ js/return/return.js
@@ -0,0 +1,10 @@
+function cutHalf(number){
+    const half = number/2;
+    return half;
+}
+
+const result = cutHalf(16);
+console.log(result);
+
+const result2 = cutHalf(result);
+console.log(result2);
