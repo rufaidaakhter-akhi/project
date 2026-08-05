f268083a6b9a1967396d3e12d10627d9082e43fd
rufaida akhter akhi <rufaida.formal@gmail.com>
2026-08-05 21:19:46 +0600

    console.log(receiptGenerator(apple, 100, 5)); console.log(receiptGenerator(pen, 200, 5)); console.log(receiptGenerator(book, 600, 5)); console.log(receiptGenerator(ruler, 800, 5)); console.log(receiptGenerator(pin, 10, 5));

--- js/m18/temp.js
+++ js/m18/temp.js
@@ -1,6 +1,9 @@
-function receiptGenerator(name, price, quy){
-    return `$name x $(quy) = $(price*qut) Taka`;
-
+function receiptGenerator(name, price, qty){
+    return `${name} x ${qty} = ${price * qty} Taka`
 }
 
 console.log(receiptGenerator("apple", 100, 5));
+console.log(receiptGenerator("pen", 200, 5));
+console.log(receiptGenerator("book", 600, 5));
+console.log(receiptGenerator("ruler", 800, 5));
+console.log(receiptGenerator("pin", 10, 5));
