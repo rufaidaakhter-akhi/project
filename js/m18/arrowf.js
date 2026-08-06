663cbb50ef4103c8162577603bebe5cb22eb4cbc
rufaida akhter akhi <rufaida.formal@gmail.com>
2026-08-06 09:13:12 +0600

    arrow function

--- js/m18/arrowf.js
+++ js/m18/arrowf.js
@@ -11,3 +11,10 @@ const calculateShipping=(orderAmount, shippingFee =60 )=>{
 
 
 console.log(calculateShipping(1000));
+console.log(calculateShipping(400));
+console.log(calculateShipping(5600));
+console.log(calculateShipping(900));
+console.log(calculateShipping(760));
+console.log(calculateShipping(7770));
+console.log(calculateShipping(670));
+console.log(calculateShipping(1000));
