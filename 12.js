25daa485baeeb0fe82b2cb4f9b41a281445d967f
rufaida akhter akhi <rufaida.formal@gmail.com>
2026-08-08 16:20:06 +0600

    nav done

---
+++ 12.js
@@ -0,0 +1,7 @@
+let sum = 0;
+
+for (let i = 1; i <= 15; i += 1) {
+    if(i%2===1){console.log(i);}
+}
+
+;
---
+++ js/exercise .js
@@ -0,0 +1,7 @@
+let sum = 0;
+
+for (let i = 2; i <= 20; i += 2) {
+    sum = sum + i;
+}
+
+console.log(sum);
