bc351b07e54593fa71ccf555a35160afff3f213d
rufaida akhter akhi <rufaida.formal@gmail.com>
2026-08-18 10:35:44 +0600

    let name

--- ts/new.js
+++ ts/new.js
@@ -1,8 +1,3 @@
 "use strict";
-let a = 12;
-let b = 12;
-let c = 12;
-let d = 12;
-let min = a - b;
-let sum = a + b;
-console.log(sum, min);
+let name1 = 'akhi';
+console.log(name1);
--- ts/new.ts
+++ ts/new.ts
@@ -1,9 +1,2 @@
-let a = 12;
-let b = 12;
-let c = 12;
-let d = 12;
-let min = a-b;
-let sum = a+b;
-let dd = 12;
-let d1 = 12;
-console.log(min);
+let name1: string ='akhi'
+console.log(name1);
